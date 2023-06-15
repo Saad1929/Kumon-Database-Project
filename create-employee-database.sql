33fa3dd5f63f9ac280dd171a79989fa144154743
Saad1929 <saad2baig123@gmail.com>
2023-06-16 00:02:34 +0100

    Employee Database and Table created.

---
+++ create-employee-database.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS `kumon_employees`;
+CREATE DATABASE `kumon_employees`;
+
+USE `kumon_employees`;
+
+CREATE TABLE `offices` ( 
+      `office_id` int(11) NOT NULL,
+      `address` varchar(50) NOT NULL,
+      `city` varchar(50) NOT NULL,
+      `postcode` varchar(10) NOT NULL,
+      PRIMARY KEY (`office_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `offices` VALUES (1,"Albertslund Hall Westacres Rd","Glasgow", "G77 6WW");
+
+
