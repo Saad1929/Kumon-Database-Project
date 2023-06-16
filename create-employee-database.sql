0200ff4ea502a49725b2d4855ca418b5b3b60711
Saad1929 <saad2baig123@gmail.com>
2023-06-16 23:35:03 +0100

    Employee Table Fully Working

--- create-employee-database.sql
+++ create-employee-database.sql
@@ -10,6 +10,19 @@ CREATE TABLE `offices` (
       `postcode` varchar(10) NOT NULL,
       PRIMARY KEY (`office_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO `offices` VALUES (1,"","Glasgow");
+INSERT INTO `offices` VALUES (1,"Albertslund Hall Westacres Rd","Glasgow", "G77 6WW");
 
-
+CREATE TABLE `employees` (
+  `employee_id` int(5) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `job_title` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `reports_to` int(11) DEFAULT NULL,
+  `office_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `fk_employees_offices_idx` (`office_id`),
+  KEY `fk_employees_employees_idx` (`reports_to`),
+  CONSTRAINT `fk_employees_managers` FOREIGN KEY (`reports_to`) REFERENCES `employees` (`employee_id`),
+  CONSTRAINT `fk_employees_offices` FOREIGN KEY (`office_id`) REFERENCES `offices` (`office_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
