73a7fec5e613c6fc733ab7cc1388ef02300d6802
Saad1929 <saad2baig123@gmail.com>
2023-06-17 23:26:57 +0100

    Parent Table Population.

--- create-kumon-store-database.sql
+++ create-kumon-store-database.sql
@@ -32,3 +32,20 @@ INSERT INTO `parents` VALUES (7,'Ilene','Dowson','1979-08-30','07669-537-908','5
 INSERT INTO `parents` VALUES (8,'Thacher','Naseby','1993-07-17','07334-890-998','53 Mosinee Center','Glasgow','G46 6AD');
 INSERT INTO `parents` VALUES (9,'Romola','Rumgay','1981-05-23','07322-222-987','33 Great Trail','Glasgow','G32 OBJ');
 INSERT INTO `parents` VALUES (10,'Levy','Mynett','1978-10-13','07322-421-228','68 Lawn Avenue','Glasgow','G32 ODT');
+INSERT INTO `parents` VALUES (11,'Thomas','Boschner','1978-11-12','07321-121-715','61 Lawn Avenue','Glasgow','G32 ODT');
+INSERT INTO `parents` VALUES (12,'Teddy','Wiggall','1992-11-04','07312-311-112','8 Manley Drive','Glasgow','G32 ODT');
+INSERT INTO `parents` VALUES (13,'Arel','Haberson','1975-02-12','07321-285-718','25 Winters Junction','Glasgow','G46 6BY');
+INSERT INTO `parents` VALUES (14,'Stefan','Herock','1991-03-28','0711-314-908','78 Sage Square','Glasgow','G46 6AD');
+INSERT INTO `parents` VALUES (15,'Dana','Findlay','1987-05-29','07311-331-908','29 Great Trail','Glasgow','G32 OBJ');
+INSERT INTO `parents` VALUES (16,'Bellina','Gladback','1979-11-20','07213-113-211','10 Manley Way','Glasgow','G32 1BT');
+INSERT INTO `parents` VALUES (17,'Violante','Salzar','1977-07-18','07649-223-889','9 Spohn Circle','Glasgow','G32 OBJ');
+INSERT INTO `parents` VALUES (18,'Kevin','Vice','1980-09-22','07321-123-908','30 Small Trail','Glasgow','G46 7IP');
+INSERT INTO `parents` VALUES (19,'Conroy','Coogan','1990-05-12','07345-546-908','1 Great Trail','Glasgow','G32 OBJ');
+INSERT INTO `parents` VALUES (20,'Mia','Bollom','1980-07-12','07320-792-922','22 Winters Junction','Glasgow','G46 6BY');
+
+
+
+
+
+
+
